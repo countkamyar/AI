6e288f5a9989e2c808a7861aa2b5d01db711b8e4
countkamyar <kamyar21rostami@gmail.com>
2019-04-09 17:51:42 +0430

    Add files via upload

---
+++ Hill_Climbing_Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{    //Kamyar Rostami AI exercise(HillclimbingAlgorithm) Mr. sheikolharam SN. 9311214023
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HillclimbingAlgorithm zx =new HillclimbingAlgorithm();
+            zx.climb();
+           
+        }
+    }
+}
