08984255c897c6fb51abc2035dac808a019d5b23
countkamyar <kamyar21rostami@gmail.com>
2019-04-09 17:50:06 +0430

    Add files via upload

---
+++ Forward_Chain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forward_Chaining_Algorithm
+{
+    class Forward_Chain
+    
+    {
+        public  Forward_Chain(char[,]se,int ab,int ac,string [] bb)
+        {
+            char[,] sentt = new char[ab, ac+1];
+            char[] s2 = new char[bb.Length+ab];
+
+            for (int i = 0; i < ab; i++)
+            {
+                for (int j = 0; j < ac+1; j++)
+                {
+                    sentt[i, j] = se[i, j];
+
+                }
+
+            }//ایجاد ماتریسی از گزاره های دریافتی
+            for (int k = 0; k < bb.Length ; k++)
+            {
+                s2[k] = Convert.ToChar(bb[k]);
+
+            }//ایجاد آرایه ای از حقایق دریافتی
+            int id = 0;
+            for (int m=0;m< ab; m++)
+            {
+                int cc=0;
+                
+                for(int n=0;n< ac; n++)
+                {//دو شرط اول برای حرکت روی ماتریس و شرط سوم برای 
+                 //مقایسه مقدار ماتریس با تک تک مقادیر آرایه حقایق است و در صورت برابر بودن یک واحد به شمارنده افزوده می گردد
+                    bool b = false;
+                    for (int o = 0; o <s2.Length; o++)
+                    {
+                        
+                        if (s2[o] == sentt[m, n])
+                        {
+                            b = true;
+                            cc++;
+
+                        }
+                    }
+                  //  if (b == false)
+                  //      break;
+                }
+                if (cc == ac)
+                {
+                    //درصورت برابر بودن مقدار شمارنده با تعداد شرط ها (صحیح بودن تمامی شروط) برنامه
+                    //مشخص می کند که این قانون جدید از کدام جمله استنتاج شده است و به مجموعه قوانین برای استنتاج مراحل بعد اضافه می کند
+                    s2[bb.Length + id] = sentt[m, ac];
+                    Console.WriteLine(sentt[m,ac]+"literal has been concluded from"+m+"sentence"+"\t\n"+"نتیجه شده است"+m + "از جمله" + sentt[m, ac] );
+                    id++;
+
+                }
+            }
+            
+        }
+     
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,15 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication8
-{    //Kamyar Rostami AI exercise(HillclimbingAlgorithm) Mr. sheikolharam SN. 9311214023
+namespace Forward_Chaining_Algorithm
+{
     class Program
-    {
+    { //Kamyar_Rostami_AIProject_S.N:9311214023
         static void Main(string[] args)
         {
-            HillclimbingAlgorithm zx =new HillclimbingAlgorithm();
-            zx.climb();
-           
+            Console.WriteLine("Kamyar_Rostami_AIProject_S.N:9311214023_Mr.sheikholharam" + "\r\n");
+            Boolean b = false;
+            
+              //  Console.WriteLine("Menu" + "\r\n" + "press the number to choose" + "\r\n" + "1.Add Sentence(Horn Clause)" + "\r\n" + "2.Add literal(fact)" + "\r\n" + "3.Infer" + "\r\n");
+               // int a = Convert.ToInt32(Console.ReadLine());
+               
+                    Console.WriteLine("enter the number of sentences you want to add:" + "\r\n" + " تعداد گزاره های دریافتی را وارد نمایید");
+                    int ab = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("enter the number of premise literals of the sentences you want to add:" + "\r\n" + " تعداد فرض های دریافتی را وارد نمایید");
+                    int ii = 0;
+                    int ac = Convert.ToInt32(Console.ReadLine());
+                   
+                    
+                    char[,] aa = new char[ab,ac + 1];
+                    while (ii < ab)
+                    {
+                        Console.WriteLine("enter the" + ii + "sentence literas one by one then hit enter");
+
+                        for (int j = 0; j < ac + 1; j++)
+                        {
+                            aa[ii,j] = Convert.ToChar(Console.ReadLine());
+
+                        }
+                        ii++;
+                    }
+                
+                
+                        Console.WriteLine("enter the number of facts you want to add:" + "\r\n" );
+                        int ba= Convert.ToInt32(Console.ReadLine());
+                   
+                    string[] bb = new string[ba];
+                        for (int n=0; n < ba;n++)
+                        {
+                            Console.WriteLine("enter the" + n + "fact");
+
+                                bb[n] = Console.ReadLine();
+
+                        
+                           
+                        }
+            Forward_Chain s1 = new Forward_Chain(aa, ab, ac,bb);
+
+
+
+
+
+
         }
     }
 }
